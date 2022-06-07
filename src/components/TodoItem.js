3db6b339ec61c7543a4fd2ab2354144c1a0e8195
hieulb2432 <hieu.lb2432@gmail.com>
2022-06-07 14:03:12 +0000

    Change Todo Status

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,11 +4,14 @@
 　・チェックボックスにチェックが入っているか管理する
 　・チェックボックスにチェックが入っているかアイテムをグレーアウトする
 */
+import React, { useState } from 'react';
 function TodoItem({item}) {
+  const [itemCss,setItemCss] = React.useState("");
+  const handleItem = () => setItemCss(itemCss == "" ? "has-text-grey-light" : "");
   return (
     <label className="panel-block">
-       <input type="checkbox" />
-      {item.text}
+      <input type="checkbox" onClick={handleItem}/>
+       <p className={itemCss}>{item.text}</p>
     </label>
   );
 }
